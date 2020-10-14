07ad07981cec3d1faa9a6c0c08985acb131977cf
RaseL Mahmud <42914006+rasel-reactive@users.noreply.github.com>
2020-10-15 01:06:40 +0600

    Update keys.js

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,9 +1,4 @@
-// module.exports = {
-//   mongoURI: "mongodb://localhost/step-react-node-api",
-//   googelClientSecret : "kuO0riyT8mywX_cfBvKw8A2r",
-//   googelClientID : "169802712538-gpu37tkikd5cuggovtgr30pfm22irbeb.apps.googleusercontent.com",
-//   cookieSecret: "FFF:DFOie0fieffffvccc"
-// }
+
 
 if (process.env.NODE_ENV === "production") {
   // we are in production -> return the prod set of keys
